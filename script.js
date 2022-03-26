d3fa44295664c6578342134376b81cb91a536390
Lauren A <laurenagraphicdesign@gmail.com>
2022-03-26 01:23:54 -0400

    Intial prompt code done, character type confirmation code done, generator code next

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var confirmNum
+var confirmCharacter
+var confirmUppercase
+var confirmLowercase
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,5 +15,38 @@ function writePassword() {
 
 }
 
+
+
+
+function generatePassword(){
+
+
+let passwordInput = prompt ("Length?")
+
+if(passwordInput < 8 || passwordInput > 128) {
+
+
+prompt ("More than 8, Less than 128") 
+
+} else {
+  confirmNum =       confirm("Numbers");
+  confirmCharacter = confirm("Characters");
+  confirmUppercase = confirm("Uppercase");
+  confirmLowercase = confirm("Lowercase");
+};
+
+
+//unsure of how to loop back to confirmation
+if (!confirmNum && !confirmCharacter && !confirmUppercase && !confirmLowercase){
+
+        alert ("One character type must be selcted")
+}
+
+
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
