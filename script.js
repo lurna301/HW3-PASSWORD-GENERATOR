e536a47966f8dba2a9efd75066ff5ee84963274b
Lauren A <laurenagraphicdesign@gmail.com>
2022-03-29 01:22:56 -0400

    changed if statements for password lenth to while loop, changed name of input variable, added parsInt to input variable, added variable to hold character selections, added random number generator

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var charSelections
+var passwordLength
 var confirmNum
-var confirmCharacter
+var confirmChar
 var confirmUppercase
 var confirmLowercase
+
+//Number, symbols, chars
+
+var passwordChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var passwordNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+//Creating an Array to return lowercase letters by generating keycodes 
+var charCodes = Array.from(Array(26)).map( (_, i) => i + 97);
+
+//Converting keycodes to string
+var lowercaseLetters = charCodes.map(code => String.fromCharCode(code));
+
+// Uppercase Letter Conversion
+var uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase());
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -18,32 +36,80 @@ function writePassword() {
 
 
 
-function generatePassword(){
+function generatePassword(passwordLength, confirmNum, confirmCharacter, confirmUppercase, confirmLowercase){
 
 
-let passwordInput = prompt ("Length?")
+  // using parseInt function to ensure user input is passed through as an interger
+ 
+     passwordLength = parseInt(prompt("Length of your password?"));
 
-if(passwordInput < 8 || passwordInput > 128) {
+  
+     // Checking Conditions for password length
+
+     while (passwordLength < 8 || passwordLength > 128){
+
+      passwordLength = parseInt(prompt("Password length must be between 8 & 128 characters."));
+
+    } 
+    
+    
+    //Start Prompts for Character Choice
 
 
-prompt ("More than 8, Less than 128") 
+    confirmNum =  confirm("Numbers");
+    confirmChar = confirm("Characters");
+    confirmUppercase = confirm("Uppercase");
+    confirmLowercase = confirm("Lowercase");
+ 
+     //switched promt to alert
+     //If time allows, create loop for character choice to include condition check 
 
-} else {
-  confirmNum =       confirm("Numbers");
-  confirmCharacter = confirm("Characters");
-  confirmUppercase = confirm("Uppercase");
-  confirmLowercase = confirm("Lowercase");
-};
+ if (!confirmNum && !confirmCharacter && !confirmUppercase && !confirmLowercase){
+
+  passwordLength = parseInt(alert("At leat one criteria must be chosen"));
+  
+
+  }
+      
+  
+
+// I researched a neater way of storing and iterating through the available characters
+// Found the below process using ternary operators
+//Adding variable to store array of all usable characters and conditionally adding properties to array
+//Each property uses spread operator
+//If selction is true then access corresponding array, if false create an empty array
+//This is in lieu of writing separate if statements, for each combination of character selections, that concatinate the variables storing the arrays of characters that will makeup the password 
 
 
-//unsure of how to loop back to confirmation
-if (!confirmNum && !confirmCharacter && !confirmUppercase && !confirmLowercase){
 
-        alert ("One character type must be selcted")
+ var availChar  = [
+
+  ...(confirmNum ? passwordNum : []),
+  ...(confirmChar ? passwordChar : []),
+  ...(confirmUppercase ? uppercaseLetters : []),
+  ...(confirmLowercase ? lowercaseLetters : []),
+
+ ];
+
+
+ // Declared variable to return password as a string
+
+var password = "";
+
+//starting random number generator
+ 
+ for (var i = 0; i < passwordLength; i++) {
+  var passwordInput = Math.floor(Math.random() * availChar.length);
+  password += availChar[passwordInput];
+ 
+
+}
+
+return password;
+
 }
 
 
-}
 
 
 // Add event listener to generate button
